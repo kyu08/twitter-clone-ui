3f8ac3fb5c99bffc15fd286f640bcf4f9daa2fde
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-06 22:37:08 +0900

    WIP modify Profile

---
+++ src/domain/models/User/Profile.ts
@@ -0,0 +1,43 @@
+import { IProfile } from './IProfile';
+import { TODO } from '../../../util/Util';
+
+export default class Profile implements IProfile {
+  // todo add each validation
+  readonly bio: TODO<'Bio'>;
+
+  readonly birthday: TODO<'Birthday'>;
+
+  readonly headerImage: TODO<'HeaderImage'>;
+
+  readonly location: TODO<'Location'>;
+
+  // @hoge の hoge の部分
+  readonly screenName: TODO<'ScreenName'>;
+
+  readonly userName: TODO<'UserName'>;
+
+  readonly userImage: TODO<'UserImage'>;
+
+  readonly website: TODO<'Website'>;
+
+  constructor(props: IProfile) {
+    const {
+      bio,
+      birthday,
+      headerImage,
+      location,
+      screenName,
+      userImage,
+      userName,
+      website,
+    } = props;
+    this.bio = bio;
+    this.birthday = birthday;
+    this.headerImage = headerImage;
+    this.location = location;
+    this.screenName = screenName;
+    this.userImage = userImage;
+    this.userName = userName;
+    this.website = website;
+  }
+}
