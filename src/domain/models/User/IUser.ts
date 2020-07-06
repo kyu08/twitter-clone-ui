84e2e0b902b992bcc77e138aac217602ecb3088f
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-06 22:36:44 +0900

    modify IUser

--- src/domain/models/User/IUser.ts
+++ src/domain/models/User/IUser.ts
@@ -2,12 +2,9 @@ import { TODO } from '../../../util/Util';
 
 // todo value object 使おう
 export interface IUser {
-  readonly UUID: TODO<'UUID'>;
+  // todo userId どうやって生成しよう
   readonly userId: TODO<'userId'>;
-  readonly userName: TODO<'userName'>;
-  readonly userImage: TODO<'userImage'>;
-  readonly headerImage: TODO<'headerImage'>;
   readonly following: Set<TODO<'userId'>>;
   readonly follower: Set<TODO<'userId'>>;
-  readonly bio: TODO<'bio'>;
+  readonly profile: TODO<'profile'>;
 }
