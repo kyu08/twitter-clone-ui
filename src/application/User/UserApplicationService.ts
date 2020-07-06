5fc4bfc41cc3419ba16da6650792a61cab269737
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-06 23:54:50 +0900

    Introduction Bio, Site

--- src/application/User/UserApplicationService.ts
+++ src/application/User/UserApplicationService.ts
@@ -1,14 +1,15 @@
 import Profile from '../../domain/models/User/Profile';
 import { IProfile } from '../../domain/models/User/IProfile';
 import Bio from '../../domain/models/User/Bio';
+import Site from '../../domain/models/User/Site';
 
 export default class UserApplicationService {
   static test() {
     const props: IProfile = {
-      bio: 'jpoge',
+      bio: new Bio('hoeg'),
       birthday: 'jpoge',
       headerImage: 'jpoge',
-      location: 'jpoge',
+      site: new Site('hoge'),
       screenName: 'jpoge',
       userName: 'jpoge',
       userImage: 'jpoge',
@@ -17,7 +18,7 @@ export default class UserApplicationService {
     const hoge = new Profile(props);
     console.log(hoge);
 
-    const bio = new Bio('hogehogehogehogehoge');
+    const bio = new Bio('hoge');
     console.log(bio);
   }
 }
