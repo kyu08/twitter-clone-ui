e502841530c30f032117ac749afe07ac44daf8ed
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-06 22:35:37 +0900

    modify User.ts

--- src/domain/models/User/User.ts
+++ src/domain/models/User/User.ts
@@ -1,6 +1,20 @@
 import { IUser } from './IUser';
+import { TODO } from '../../../util/Util';
 
 export class User implements IUser {
-  // constructor() {
-  // }
+  readonly userId: TODO<'userId'>;
+
+  readonly following: TODO<'following'>;
+
+  readonly follower: TODO<'follower'>;
+
+  readonly profile: TODO<'profile'>;
+
+  constructor(props: IUser) {
+    const { follower, following, profile, userId } = props;
+    this.follower = follower;
+    this.following = following;
+    this.profile = profile;
+    this.userId = userId;
+  }
 }
