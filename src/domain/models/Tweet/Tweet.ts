805648a152832bf067fe31805e1d98962c44762d
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-09 00:43:49 +0900

    add Tweet.ts

---
+++ src/domain/models/Tweet/Tweet.ts
@@ -0,0 +1,51 @@
+import { AbstractTweet } from './AbstractTweet';
+import UserId from '../User/UserId';
+
+interface Tweetprops {
+  readonly tweetId: TweetId;
+  readonly userId: UserId;
+  readonly content: Content;
+  readonly retweetMap: RetweetMap;
+  readonly likeSet: LikeSet;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
+
+export default class Tweet extends AbstractTweet {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props: Tweetprops) {
+    super(props);
+  }
+
+  like(): any {
+    console.log(1);
+  }
+
+  cancelLike(): any {
+    console.log(1);
+  }
+
+  retweet(): any {
+    console.log(1);
+  }
+
+  cancelRetweet(): any {
+    console.log(1);
+  }
+
+  returnUpdatedInstance(): any {
+    console.log(1);
+  }
+}
+
+const props = {
+  tweetId: 0,
+  userId: 0,
+  content: 0,
+  retweetMap: 0,
+  likeSet: 0,
+  createdAt: 0,
+  updatedAt: 0,
+};
+const tweet = new Tweet(props);
+console.log(tweet);
