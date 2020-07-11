31a1f7bfa94df751c0e77a26694c3d8af82e8dd2
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-11 16:07:22 +0900

    modify RetweetMap -> IRetweetMap

--- src/domain/models/Tweet/RetweetMap/IRetweetMap.ts
+++ src/domain/models/Tweet/RetweetMap/IRetweetMap.ts
@@ -1,9 +1,8 @@
 import UserId from '../../User/UserId/UserId';
-import RetweetMap from './RetweetMap';
 
 export interface IRetweetMap {
   // todo ここ Date だと不十分？ 値オブジェクト作った方がいい？
   readonly retweetMap: Map<UserId, Date>;
-  retweet(userId: UserId): RetweetMap;
-  cancelRetweet(userId: UserId): RetweetMap;
+  retweet(userId: UserId): IRetweetMap;
+  cancelRetweet(userId: UserId): IRetweetMap;
 }
