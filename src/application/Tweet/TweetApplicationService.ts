2a42d2fc2cd8079666e8b933a783d76e74f1cd60
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-11 13:31:28 +0900

    modify ~application

--- src/application/Tweet/TweetApplicationService.ts
+++ src/application/Tweet/TweetApplicationService.ts
@@ -1,9 +1,9 @@
-import Tweet from '../../domain/models/Tweet/Tweet';
-import LikeSet from '../../domain/models/Tweet/LikeSet';
-import TweetId from '../../domain/models/Tweet/TweetId';
-import UserId from '../../domain/models/User/UserId';
-import Content from '../../domain/models/Tweet/Content';
-import RetweetMap from '../../domain/models/Tweet/RetweetMap';
+import Tweet from '../../domain/models/Tweet/ConcreteClasses/Tweet';
+import LikeSet from '../../domain/models/Tweet/LikeSet/LikeSet';
+import TweetId from '../../domain/models/Tweet/TweetId/TweetId';
+import UserId from '../../domain/models/User/UserId/UserId';
+import Content from '../../domain/models/Tweet/Content/Content';
+import RetweetMap from '../../domain/models/Tweet/RetweetMap/RetweetMap';
 
 export class TweetApplicationService {
   static test() {
@@ -25,9 +25,11 @@ export class TweetApplicationService {
     };
     const tweet = new Tweet(props);
     const userA = new UserId(1234);
-    const tweetB = tweet.like(userA);
-    const userB = new UserId(2345);
-    const tweetC = tweetB.like(userB);
+    // const userB = new UserId(12344);
+    const tweetB = tweet.retweet(userA);
+    const tweetC = tweetB.cancelRetweet(userA);
+    console.log(tweetB);
+    // const tweetC = tweetB.cancelLike(userA);
     console.log(tweetC);
   }
 }
