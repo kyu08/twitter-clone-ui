39b4576fae502ca26457078872374c3b1458787f
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-11 16:04:08 +0900

    modify LikeSet -> ILikeSet

--- src/domain/models/Tweet/LikeSet/ILikeSet.ts
+++ src/domain/models/Tweet/LikeSet/ILikeSet.ts
@@ -1,9 +1,8 @@
 import UserId from '../../User/UserId/UserId';
-import LikeSet from './LikeSet';
 
 export interface ILikeSet {
   // todo ここ Date だと不十分？ 値オブジェクト作った方がいい？
   readonly likeSet: Set<UserId>;
-  like(userId: UserId): LikeSet;
-  cancelLike(userId: UserId): LikeSet;
+  like(userId: UserId): ILikeSet;
+  cancelLike(userId: UserId): ILikeSet;
 }
