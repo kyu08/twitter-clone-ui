681c3780595eb1a656ed14e439e67d3edfd21c10
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-13 18:08:01 +0900

    add Container, Header, Timeline, Footer(cherry-picked)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-// import { TweetApplicationService } from './application/Tweet/TweetApplicationService';
 import UserApplicationService from './application/User/UserApplicationService';
-// import UserId from './domain/models/User/UserId/UserId';
 import { Container } from './container/Container';
 
 const App = () => {
-  // TweetApplicationService.test();
   UserApplicationService.test();
 
   return <Container />;
