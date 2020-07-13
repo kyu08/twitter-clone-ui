7ec660bb3be573c9379a3b19b521683e239e1ac1
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 00:14:12 +0900

    add Tweet.tsx

---
+++ src/components/Timeline/Tweet.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import classes from './Tweet.module.css';
+
+export const Tweet: React.FC<{}> = () => {
+  return <div className={classes.Tweet}>this is tweet.</div>;
+};
