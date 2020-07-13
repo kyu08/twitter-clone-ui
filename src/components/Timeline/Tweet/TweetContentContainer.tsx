cbdd266b97c61b2c43476907076a840d0bfcb554
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 03:44:28 +0900

    delete いったん TweetImage はなしで

--- src/components/Timeline/Tweet/TweetContentContainer.tsx
+++ src/components/Timeline/Tweet/TweetContentContainer.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import classes from './TweetContentContainer.module.css';
 import { TweetContent } from './TweetContentContainer/TweetContent';
-import { TweetImage } from './TweetContentContainer/TweetImage';
 import { TweetButtonContainer } from './TweetContentContainer/TweetButtonContainer';
 
 export const TweetContentContainer: React.FC<{}> = () => {
   return (
     <div className={classes.TweetContentContainer}>
       <TweetContent content="にゃ〜ん" />
-      <TweetImage />
       <div className={classes.TweetButtonsWrapper}>
         {/* todo map で書く*/}
         <TweetButtonContainer buttonTitle="Reply" />
