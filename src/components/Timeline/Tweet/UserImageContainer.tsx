a5c923dac9ee3dd647d4c9f7bcc9eee43a3fc346
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 03:28:19 +0900

    modify TweetImage

--- src/components/Timeline/Tweet/UserImageContainer.tsx
+++ src/components/Timeline/Tweet/UserImageContainer.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import classes from './UserImageContainer.module.css';
 
 export const UserImageContainer: React.FC<{}> = () => {
-  return <div className={classes.UserImageContainer}>UserImageContainer</div>;
+  return <div className={classes.UserImageContainer}>UserImage</div>;
 };
