1034d1fa1b4dce3df273f5e92786bee35e2797eb
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 03:28:04 +0900

    add TweetInformation

--- src/components/Timeline/Tweet/TweetInformationContainer.tsx
+++ src/components/Timeline/Tweet/TweetInformationContainer.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import classes from './TweetInformationContainer.module.css';
+import { UserName } from './TweetInformationContainer/UserName';
+import { ScreenName } from './TweetInformationContainer/ScreenName';
+import { HowLongAgo } from './TweetInformationContainer/HowLongAgo';
+import { TweetMenuButton } from './Common/TweetMenuButton';
 
 export const TweetInformationContainer: React.FC<{}> = () => {
   return (
     <div className={classes.TweetInformationContainer}>
-      TweetInformationContainer
+      <UserName userName="ねこ" />
+      <ScreenName screenName="i_am_cat" />
+      <HowLongAgo howLongAgo={10} />
+      {/* todo ↓こいつは右揃え */}
+      <div className={classes.TweetOptionButtonWrapper}>
+        <TweetMenuButton buttonTitle="+" />
+      </div>
     </div>
   );
 };
