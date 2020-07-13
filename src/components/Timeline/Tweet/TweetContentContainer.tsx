37db7d59dcb4f0ab24df054ae164e29448a50afa
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 03:27:44 +0900

    add TweetContents

--- src/components/Timeline/Tweet/TweetContentContainer.tsx
+++ src/components/Timeline/Tweet/TweetContentContainer.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import classes from './TweetContentContainer.module.css';
+import { TweetContent } from './TweetContentContainer/TweetContent';
+import { TweetImage } from './TweetContentContainer/TweetImage';
+import { TweetButtonContainer } from './TweetContentContainer/TweetButtonContainer';
 
 export const TweetContentContainer: React.FC<{}> = () => {
   return (
-    <div className={classes.TweetContentContainer}>TweetContentContainer</div>
+    <div className={classes.TweetContentContainer}>
+      <TweetContent content="にゃ〜ん" />
+      <TweetImage />
+      <div className={classes.TweetButtonsWrapper}>
+        {/* todo map で書く*/}
+        <TweetButtonContainer buttonTitle="Reply" />
+        <TweetButtonContainer buttonTitle="RT" />
+        <TweetButtonContainer buttonTitle="Like" />
+        <TweetButtonContainer buttonTitle="Share" />
+      </div>
+    </div>
   );
 };
