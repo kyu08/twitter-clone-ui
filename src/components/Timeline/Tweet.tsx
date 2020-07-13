10ba9d12fc4bd39836d775de72cdf8523333d9b7
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 00:27:39 +0900

    add TweetInformationContainer.tsx

--- src/components/Timeline/Tweet.tsx
+++ src/components/Timeline/Tweet.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import classes from './Tweet.module.css';
+import { UserImageContainer } from './Tweet/UserImageContainer';
+import { TweetInformationContainer } from './Tweet/TweetInformationContainer';
+import { TweetContentContainer } from './Tweet/TweetContentContainer';
 
 export const Tweet: React.FC<{}> = () => {
-  return <div className={classes.Tweet}>this is tweet.</div>;
+  return (
+    <div className={classes.Tweet}>
+      <UserImageContainer />
+      <TweetInformationContainer />
+      <TweetContentContainer />
+      this is tweet.
+    </div>
+  );
 };
---
+++ src/components/Timeline/Tweet/TweetInformationContainer.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import classes from './TweetInformationContainer.module.css';
+
+export const TweetInformationContainer: React.FC<{}> = () => {
+  return (
+    <div className={classes.TweetInformationContainer}>
+      TweetInformationContainer
+    </div>
+  );
+};
