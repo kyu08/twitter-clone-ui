7a25e7a48c4b61a8d4fb604656dcd922e177cbde
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-14 00:27:22 +0900

    add TweetContentContainer.tsx

---
+++ src/components/Timeline/Tweet/TweetContentContainer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import classes from './TweetContentContainer.module.css';
+
+export const TweetContentContainer: React.FC<{}> = () => {
+  return (
+    <div className={classes.TweetContentContainer}>TweetContentContainer</div>
+  );
+};
