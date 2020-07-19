4585d15e4811a1afff32bf13f8bc8c41b6ce660b
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-19 14:20:38 +0900

    add Logo, Message

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import classes from './Login.module.css';
+import { Logo } from './Login/Logo';
+import { Message } from './Login/Message';
 
 export const Login: React.FC<{}> = () => {
-  return <div className={classes.Login}>Shimatterにログイン</div>;
+  return (
+    <div className={classes.Login}>
+      <Logo />
+      <Message />
+    </div>
+  );
 };
---
+++ src/components/Login/Logo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import classes from './Logo.module.css';
+import TwitterLogo from './TwitterLogo.png';
+
+export const Logo: React.FC<{}> = () => {
+  return (
+    <div className={classes.Logo}>
+      <img className={classes.LogoImage} src={TwitterLogo} />
+    </div>
+  );
+};
