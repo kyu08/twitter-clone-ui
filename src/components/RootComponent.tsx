435de6f5fb5cb818a52001c1b988447d57e4ef4d
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-20 13:42:11 +0900

    modify routing ちゃんと redirect するように修正

--- src/components/RootComponent.tsx
+++ src/components/RootComponent.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Home } from './Home';
 import { Login } from './Login';
 import classes from './RootComponent.module.css';
 
 export const RootComponent: React.FC<{}> = () => {
   const [isLogin, setIsLogin] = React.useState<boolean>(false);
+
   useEffect(() => {
     const userIdInLocalStorage = localStorage.getItem('userId');
     if (userIdInLocalStorage) setIsLogin(true);
@@ -17,8 +18,8 @@ export const RootComponent: React.FC<{}> = () => {
       <BrowserRouter>
         <Route path="/login" component={Login} />
         <Route path="/home" component={Home} />
+        {isLogin ? <Redirect to="/home" /> : <Redirect to="/login" />}
       </BrowserRouter>
-      {isLogin ? <Home /> : <Login />}
     </div>
   );
 };
