fd5579575d3b384b6a9b8e09384269d02695bbbe
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-31 02:03:48 +0900

    add comment

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -14,7 +14,7 @@ export const Timeline: React.FC<Props> = (props) => {
     <div className={classes.Timeline}>
       {/* todo #112 これ関数化する*/}
       {tweetArray.map((t) => (
-        // todo component が properties を知ってるイマイチ
+        // todo #116 component が properties を知ってるイマイチ
         <TweetComponent tweet={t} key={t.tweetId.tweetId} />
       ))}
     </div>
