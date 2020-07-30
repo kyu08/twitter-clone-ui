cbe5104f63bc2734e5fd82c494f4252854c4fa20
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-30 23:18:52 +0900

    modify IUser, User

--- src/domain/models/User/IUser.ts
+++ src/domain/models/User/IUser.ts
@@ -1,22 +1,15 @@
 import Profile from './Profile/Profile';
 import UserId from './UserId/UserId';
-import Following from './Following/Following';
-import Follower from './Follower/Follower';
 import { BirthdayProps } from './Profile/Birthday';
 
 export interface UserProps {
-  readonly follower: Follower;
-  readonly following: Following;
   readonly profile: Profile;
   readonly userId: UserId;
+  readonly followerCount: number;
+  readonly followingCount: number;
 }
 
 export interface IUser extends UserProps {
-  // todo follow method と同時に followed method を実行したいけどどこでやろうね -> UserApplicationService でやる
-  follow(userId: UserId): IUser;
-  unFollow(userId: UserId): IUser;
-  followed(userId: UserId): IUser;
-  unFollowed(userId: UserId): IUser;
   updateBio(bioString: string): IUser;
   updateBirthday(birthdayProps: BirthdayProps): IUser;
   returnUpdatedInstance<T extends keyof IUser>(key: T, value: IUser[T]): IUser;
