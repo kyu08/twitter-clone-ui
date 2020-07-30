24035596a7c8537accbcd8d37757ec29260a5f64
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-07-30 23:43:52 +0900

    comment out UserFactory

--- src/domain/models/User/UserFactory.ts
+++ src/domain/models/User/UserFactory.ts
@@ -18,87 +18,90 @@ import { IProps, IUserProps } from '../../../inMemory/InMemoryUsers';
 import { IUser } from './IUser';
 
 export default class UserFactory {
-  static toInstanceUserId(set: any[]): Set<UserId> {
-    // todo ださいきがする
-    const setString = JSON.stringify(set);
-    if (setString === '{}' || setString === '[]') return new Set();
-    const setProps = set.map((u) => {
-      return new UserId(u.userId);
-    });
-
-    return new Set(setProps);
+  static test() {
+    console.log(1);
   }
-
-  static createUserPropsFromJSON(propsJSON: any): IUserProps {
-    const { follower, following, userId, profile } = propsJSON;
-    const followerParsed = UserFactory.toInstanceUserId(follower.follower);
-    const followingParsed = UserFactory.toInstanceUserId(following.following);
-
-    const props = {
-      bio: profile.bio.bio,
-      day: profile.birthday.day.day,
-      follower: followerParsed,
-      following: followingParsed,
-      headerImage: profile.headerImage.headerImage,
-      month: profile.birthday.month.month,
-      screenName: profile.screenName.screenName,
-      userId: userId.userId,
-      userImage: profile.userImage.userImage,
-      userLocation: profile.userLocation.userLocation,
-      userName: profile.userName.userName,
-      website: profile.website.website,
-      year: profile.birthday.year.year,
-    };
-
-    return UserFactory.createProps(props);
-  }
-
-  static createProps(props: IProps): IUserProps {
-    return {
-      bio: new Bio(props.bio),
-      day: new Day(props.day),
-      follower: new Follower(props.follower),
-      following: new Following(props.following),
-      headerImage: new HeaderImage(props.headerImage),
-      month: new Month(props.month),
-      screenName: new ScreenName(props.screenName),
-      userId: new UserId(props.userId),
-      userImage: new UserImage(props.userImage),
-      userLocation: new UserLocation(props.userLocation),
-      userName: new UserName(props.userName),
-      website: new Website(props.website),
-      year: new Year(props.year),
-    };
-  }
-
-  static create(props: IUserProps): IUser {
-    const {
-      bio,
-      day,
-      follower,
-      following,
-      headerImage,
-      month,
-      screenName,
-      userId,
-      userImage,
-      userLocation,
-      userName,
-      website,
-      year,
-    } = props;
-    const birthday = new Birthday({ day, month, year });
-    const profile = new Profile({
-      birthday,
-      bio,
-      headerImage,
-      screenName,
-      userImage,
-      userLocation,
-      userName,
-      website,
-    });
-
-    return new User({ profile, follower, following, userId });
-  }
+  // static toInstanceUserId(set: any[]): Set<UserId> {
+  //   // todo ださいきがする
+  //   const setString = JSON.stringify(set);
+  //   if (setString === '{}' || setString === '[]') return new Set();
+  //   const setProps = set.map((u) => {
+  //     return new UserId(u.userId);
+  //   });
+  //
+  //   return new Set(setProps);
+  // }
+  //
+  // static createUserPropsFromJSON(propsJSON: any): IUserProps {
+  //   const { follower, following, userId, profile } = propsJSON;
+  //   const followerParsed = UserFactory.toInstanceUserId(follower.follower);
+  //   const followingParsed = UserFactory.toInstanceUserId(following.following);
+  //
+  //   const props = {
+  //     bio: profile.bio.bio,
+  //     day: profile.birthday.day.day,
+  //     follower: followerParsed,
+  //     following: followingParsed,
+  //     headerImage: profile.headerImage.headerImage,
+  //     month: profile.birthday.month.month,
+  //     screenName: profile.screenName.screenName,
+  //     userId: userId.userId,
+  //     userImage: profile.userImage.userImage,
+  //     userLocation: profile.userLocation.userLocation,
+  //     userName: profile.userName.userName,
+  //     website: profile.website.website,
+  //     year: profile.birthday.year.year,
+  //   };
+  //
+  //   return UserFactory.createProps(props);
+  // }
+  //
+  // static createProps(props: IProps): IUserProps {
+  //   return {
+  //     bio: new Bio(props.bio),
+  //     day: new Day(props.day),
+  //     follower: new Follower(props.follower),
+  //     following: new Following(props.following),
+  //     headerImage: new HeaderImage(props.headerImage),
+  //     month: new Month(props.month),
+  //     screenName: new ScreenName(props.screenName),
+  //     userId: new UserId(props.userId),
+  //     userImage: new UserImage(props.userImage),
+  //     userLocation: new UserLocation(props.userLocation),
+  //     userName: new UserName(props.userName),
+  //     website: new Website(props.website),
+  //     year: new Year(props.year),
+  //   };
+  // }
+  //
+  // static create(props: IUserProps): IUser {
+  //   const {
+  //     bio,
+  //     day,
+  //     follower,
+  //     following,
+  //     headerImage,
+  //     month,
+  //     screenName,
+  //     userId,
+  //     userImage,
+  //     userLocation,
+  //     userName,
+  //     website,
+  //     year,
+  //   } = props;
+  //   const birthday = new Birthday({ day, month, year });
+  //   const profile = new Profile({
+  //     birthday,
+  //     bio,
+  //     headerImage,
+  //     screenName,
+  //     userImage,
+  //     userLocation,
+  //     userName,
+  //     website,
+  //   });
+  //
+  //   return new User({ profile, follower, following, userId });
+  // }
 }
