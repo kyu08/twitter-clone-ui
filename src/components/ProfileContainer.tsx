3d5a511e1e340825abb7eb50fd7af66558136e09
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-08-16 22:04:06 +0900

    add ProfileContainer.tsx

---
+++ src/components/ProfileContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+type Props = {
+  //
+};
+
+export const ProfileContainer: React.FC<Props> = (props) => {
+  const data = props;
+
+  return <>this is ProfileContainer</>;
+};
