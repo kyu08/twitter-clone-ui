2fc33a431fc53c8da78828316f6fda66f89b0d32
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-08-26 01:21:51 +0900

    modify ensurePropsContainsNoUndefined throw error -> console.log

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -9,13 +9,13 @@ export const ensurePropsContainsNoUndefined = <T>(props: T) => {
   const values = Object.values(props);
   if (values.includes(undefined)) {
     console.log(values);
-    throw new Error('↑↑↑ this property contains undefined.');
+    console.table({ message: '↑↑↑ this property contains undefined.' });
   }
   values.forEach((value) => {
     if (!value) return;
     if (Object.values(value).includes(undefined)) {
       console.log(value);
-      throw new Error('↑↑↑ this property contains undefined.');
+      console.table({ message: '↑↑↑ this property contains undefined.' });
     }
   });
 };
