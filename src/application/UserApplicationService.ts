7d071e8286ca52c5ede1135ec6ec59116da56e14
kyu08 <kyu08@users.noreply.github.com>
2021-02-22 20:18:02 +0900

    made UserApplicationService's responsibilities appropriate

--- src/application/UserApplicationService.ts
+++ src/application/UserApplicationService.ts
@@ -24,6 +24,28 @@ export type UserPropsDetail = {
   followerMap: [string, string][];
 };
 
+const toInstanceUserId = (userIdString: string): UserId =>
+  new UserId(userIdString);
+
+const getUserJson = (userId: UserId): Promise<Response> => {
+  const userIdString = userId.userId;
+
+  return fetch(`${hostURL}/user/userId/${userIdString}/full`, {
+    mode: 'cors',
+  });
+};
+
+const getUserJsonByScreenName = (screenName: ScreenName): Promise<Response> => {
+  const screenNameString = screenName.screenName;
+
+  return fetch(
+    `${hostURL}/user/screenName/full?screenName=${screenNameString}`,
+    {
+      mode: 'cors',
+    },
+  );
+};
+
 export default class UserApplicationService {
   userFactory: UserFactory;
 
@@ -58,11 +80,11 @@ export default class UserApplicationService {
     const userIdInLocalStorage = localStorage.getItem('userId');
     if (!userIdInLocalStorage) return null;
 
-    return this.toInstanceUserId(userIdInLocalStorage);
+    return toInstanceUserId(userIdInLocalStorage);
   }
 
   async getCurrentUser(userId: UserId): Promise<UserDataModel> {
-    const userData = await this.getUserJson(userId).catch((e) => e);
+    const userData = await getUserJson(userId).catch((e) => e);
     const userJson = await userData.json();
     const user = this.userFactory.toInstance(userJson);
 
@@ -70,9 +92,7 @@ export default class UserApplicationService {
   }
 
   async getUserByScreenName(screenName: ScreenName): Promise<UserDataModel> {
-    const userData = await this.getUserJsonByScreenName(screenName).catch(
-      (e) => e,
-    );
+    const userData = await getUserJsonByScreenName(screenName).catch((e) => e);
     const userJson = await userData.json();
     const user = this.userFactory.toInstance(userJson);
 
