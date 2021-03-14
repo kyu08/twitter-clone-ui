20c7bf40a1411f84e208710a69d917b2fc91b0fd
kyu08 <kyu08@users.noreply.github.com>
2021-03-14 18:00:14 +0900

    add canSubmit test

---
+++ src/test/TempTweet.test.ts
@@ -0,0 +1,51 @@
+import { TempTweet } from '../domain/models/TempTweet/ConcreteClasses/TempTweet';
+import TempContent from '../domain/models/TempTweet/TempContent';
+import UserId from '../domain/models/User/UserId/UserId';
+
+describe('TempTweet', () => {
+  describe('canSubmit', () => {
+    test('tempContent.length = 0 のときcanSubmit が false を返す', () => {
+      const tempContent = new TempContent('');
+      const userId = new UserId('e15a1c26-9a65-4f89-91b0-99b2055ae26f');
+      const tempTweetProps = { tempContent, userId };
+      const tempTweet = new TempTweet(tempTweetProps);
+      expect(tempTweet.canSubmit()).toBe(false);
+    });
+
+    test('tempContent.length = 1 のときcanSubmit が true を返す', () => {
+      const tempContent = new TempContent('1');
+      const userId = new UserId('e15a1c26-9a65-4f89-91b0-99b2055ae26f');
+      const tempTweetProps = { tempContent, userId };
+      const tempTweet = new TempTweet(tempTweetProps);
+      expect(tempTweet.canSubmit()).toBe(true);
+    });
+
+    test('tempContent.length = 6 のときcanSubmit が true を返す', () => {
+      const tempContent = new TempContent('hello.');
+      const userId = new UserId('e15a1c26-9a65-4f89-91b0-99b2055ae26f');
+      const tempTweetProps = { tempContent, userId };
+      const tempTweet = new TempTweet(tempTweetProps);
+      expect(tempTweet.canSubmit()).toBe(true);
+    });
+
+    test('tempContent.length = 140 のときcanSubmit が true を返す', () => {
+      const tempContent = new TempContent(
+        '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890',
+      );
+      const userId = new UserId('e15a1c26-9a65-4f89-91b0-99b2055ae26f');
+      const tempTweetProps = { tempContent, userId };
+      const tempTweet = new TempTweet(tempTweetProps);
+      expect(tempTweet.canSubmit()).toBe(true);
+    });
+
+    test('tempContent.length = 141 のときcanSubmit が false を返す', () => {
+      const tempContent = new TempContent(
+        '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+      );
+      const userId = new UserId('e15a1c26-9a65-4f89-91b0-99b2055ae26f');
+      const tempTweetProps = { tempContent, userId };
+      const tempTweet = new TempTweet(tempTweetProps);
+      expect(tempTweet.canSubmit()).toBe(false);
+    });
+  });
+});
